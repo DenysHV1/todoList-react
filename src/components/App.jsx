44aee0d03f8bc4432330e55f05c2f1e0f1d386e3
DenVik <denyagarkusha2@gmail.com>
2024-10-05 13:53:17 +0200

    add fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { TodoList } from './TodoList/TodoList';
 
 export const App = () => {
   return (
-    <>
+    <div className='container'>
       <TodoList></TodoList>
-    </>
+    </div>
   );
 };
